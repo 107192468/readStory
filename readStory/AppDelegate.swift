51bd0253faadc3ab62f431b5e8decacd2f64e3bf
张 <zhangling55@wanda.cn>
2016-09-19 09:39:05 +0800

    Initial Commit

---
+++ readStory/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  readStory
+//
+//  Created by 张凌 on 16/9/19.
+//  Copyright © 2016年 张凌. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ readStory/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  readStory
+//
+//  Created by 张凌 on 16/9/19.
+//  Copyright © 2016年 张凌. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
